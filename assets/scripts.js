4379ca1b3b445ebac37a6a4c52adc52639db7c90
Leandro Bermeo <leandrofbermeo@gmail.com>
2024-07-16 00:18:57 +0100

    add second alt version

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
   $('#sidebarCollapse').on('click', function () {
-    $('.agenda-v2--sidebar-alt').toggleClass('active');
+    $('.agenda-v2--sidebar-mobile').toggleClass('active');
     $('#sidebarCollapse').toggleClass('collapsed');
+  });
+});
+
+$(document).ready(function () {
+  $('#sidebarCollapse-desktop-mobile').on('click', function () {
+    $('.agenda-v2-alt').toggleClass('collapsed');
   });
 });
 
